659e2b49aaad04f21925fdf9fd26d9e08fc2d3ae
Thanpisit <porsohan@gmail.com>
2026-02-28 16:36:34 +0000

    On branch master  Changes to be committed:        modified:   hotel-dashboard/server.js

--- hotel-dashboard/server.js
+++ hotel-dashboard/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const mqtt = require('mqtt');
+const mysql = require('mysql2');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,6 +10,43 @@ const io = new Server(server);
 
 // 1. ให้ Node.js เสิร์ฟไฟล์ HTML/CSS จากโฟลเดอร์ public
 app.use(express.static('public'));
+
+// ==========================================
+// 1. ตั้งค่าเชื่อมต่อ MySQL Database
+// ==========================================
+//const db = mysql.createPool({
+//    host: 'mysql', // วิ่งไปหา Service ชื่อ mysql ใน Docker ได้เลย
+//    user: 'root',
+//    password: 'hotel123', // ⚠️ เปลี่ยนให้ตรงกับ MYSQL_ROOT_PASSWORD ในไฟล์ .env ของคุณ
+//    database: 'hotel_db',
+//    waitForConnections: true,
+//    connectionLimit: 10,
+//   queueLimit: 0
+//});
+
+// สร้าง Table อัตโนมัติ (ถ้ายังไม่มี)
+//const initDB = () => {
+    // ตารางเก็บประวัติการใช้บัตร RFID
+//    const createRfidTable = `
+//        CREATE TABLE IF NOT EXISTS access_logs (
+//            id INT AUTO_INCREMENT PRIMARY KEY,
+//            rfid_uid VARCHAR(50) NOT NULL,
+//            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//        )
+//    `;
+    // ตารางเก็บค่ากระแสไฟจาก ACS712
+//    const createSensorTable = `
+//        CREATE TABLE IF NOT EXISTS sensor_logs (
+//            id INT AUTO_INCREMENT PRIMARY KEY,
+//            current_amp FLOAT NOT NULL,
+//            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//        )
+//    `;
+//    db.query(createRfidTable, (err) => { if(err) console.error("RFID DB Error:", err); });
+//    db.query(createSensorTable, (err) => { if(err) console.error("Sensor DB Error:", err); });
+//    console.log("🗄️ Database & Tables Ready!");
+//};
+//initDB();
 
 // 2. ตั้งค่าเชื่อมต่อ MQTT Broker (ใช้ DuckDNS หรือ IP เซิร์ฟเวอร์คุณ)
 const mqttClient = mqtt.connect('mqtt://recasa888.duckdns.org:1883', {
